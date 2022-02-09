b81b4ff5a0c590bd287ef7abd4e1113d53e0d260
vernon2gh <vernon2gm@gmail.com>
2022-02-09 09:32:27 +0800

    Add: list head file

--- types.h
+++ types.h
@@ -4,5 +4,10 @@
 typedef unsigned int gfp_t;
 
 typedef unsigned long phys_addr_t;
+typedef void *        virt_addr_t;
+
+struct list_head {
+	struct list_head *next, *prev;
+};
 
 #endif /* __TYPES_H */
