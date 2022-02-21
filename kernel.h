67915b5a092520681c3b403ec912d21a712d3814
vernon2gh <vernon2gm@gmail.com>
2022-02-21 13:48:29 +0800

    Add align macro

--- kernel.h
+++ kernel.h
@@ -66,5 +66,12 @@
 	void *__mptr = (void *)(ptr);			\
 	((type *)(__mptr - offsetof(type, member))); })
 
+#define ALIGN_MASK(x, mask)	(((x) + (mask)) & ~(mask))
+#define __ALIGN(x, a)		ALIGN_MASK(x, (typeof(x))(a) - 1)
+
+/* @a is a power of 2 value */
+#define ALIGN(x, a)		__ALIGN((x), (a))
+#define ALIGN_DOWN(x, a)	__ALIGN((x) - ((a) - 1), (a))
+
 #endif /* __KERNEL_H */
 
